96140557397f482eba1c0e49e3333ecf62cd9715
Memorias de un informático <32130278+memoriasIT@users.noreply.github.com>
2018-04-06 16:20:24 +0200

    Add files via upload

--- ARM Projects/fibonacci/fibonacci.s
+++ ARM Projects/fibonacci/fibonacci.s
@@ -1,22 +1,37 @@
 .text
 .global main
-main:   push {r3}
-        mov r0, #6      @fibonacci n = 6
-        mov r1, #0      @r1 num 1
-        mov r2, #1      @r2 num 2
-        cmp r0, #1      @ if r0 > 1 then
-        bgt fib        @ goto fib
-        b exit          @ else exit
-fib:   cmp r0, #0      @ for acabado
-        beq exit        @ exit
-        add r3, r1, r2
-        push {r3}
-        mov r1, r2
-        mov r2, r3
-        sub r0, #1      @ for -1
-        b fib          @ goto fib
+main:
+	mov r0, #6
+	bl fib
+	bx lr
 
-exit:   mov r0, r3
-        pop {r3}
-        bx lr
+fib:
+	cmp r0, #0
+	beq salir
+	cmp r0, #1
+	beq exit1
+	push {r4}
+	mov r1, #0
+	mov r2, #1
+	sub r0, #1
+
+loop:
+	cmp r0, #0
+	beq exit0
+	add r4, r1, r2
+	mov r1, r2
+	mov r2, r4
+	sub r0, #1
+	b loop
+
+exit1:
+	mov r0, #1
+	b salir
+
+exit0:
+	mov r0, r4
+	pop {r4}
+
+salir:
+	bx lr
 .end
